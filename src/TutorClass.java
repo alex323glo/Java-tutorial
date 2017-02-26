0be1c527409399d214c181b7d69307de4a2ba0a0
alex323glo <alex323glo@gmail.com>
2017-02-27 00:40:58 +0200

    Were added: 1. New class "TutorClass"; 2. main() method 3. console-print

---
+++ src/TutorClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Alexey_O on 27.02.2017.
+ */
+public class TutorClass {
+    public static void main(String[] args) {
+        System.out.println("Hello! This is my first project, published from IntellijIdea to GitHub!");
+    }
+}
